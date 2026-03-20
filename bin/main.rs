42d4b7fa461db1c1fa0f54c8c89f69adef96f2c0
znz <z3nz@pm.me>
2026-03-20 15:50:30 +0400

    sudoku solver done

--- bin/main.rs
+++ bin/main.rs
@@ -1,29 +1,22 @@
-use backpack::math::geometry::Triangle;
+use backpack::misc::sudoku::SudokuGame;
 
 fn main() {
-    let mut base = 4;
-    let mut res = 0;
-    let lim: u64 = 1e9 as u64;
-    loop {
-        let lside =  base - 1;
-        let gside = base + 1;
-        let c1 = Triangle::new(lside, lside, base);
-        let c2 = Triangle::new(gside, gside, base);
-        let p1 = lside * 2 + base;
-        let p2 = gside * 2 + base;
+    let mut ans: u32 = 0;
+    for _ in 0..50 {
+        let mut buf = String::new();
+        std::io::stdin().read_line(&mut buf).expect("failed");
 
-        if p1 <= lim && c1.is_almost_equlateral() {
-            res += p1;
-        }
-        if p2 <= lim && c2. is_almost_equlateral() {
-            res += p2;
+        let mut f: Vec<String> = vec![String::new(); 9];
+        for row in f.iter_mut() {
+            std::io::stdin().read_line(row).expect("failed");
         }
 
-        if p1 > lim && p2 > lim {
-            break;
-        }
+        let mut game = SudokuGame::from_string_vec(f);
 
-        base += 1;
+        let res = game.solve().unwrap();
+
+        ans += res[0].iter().take(3).fold(0, |acc, &x| acc * 10 + x as u32);
     }
-    println!("{res}");
+
+    println!("{ans}");
 }
--- src/misc/sudoku.rs
+++ src/misc/sudoku.rs
@@ -1,5 +1,6 @@
 pub struct SudokuGame {
     layout: [[u8; 9]; 9],
+    area_idx: [[Vec<usize>; 9]; 9],
     areas: Vec<Vec<(usize, usize)>>,
     used: Vec<u16>,
 }
@@ -9,7 +10,7 @@ impl SudokuGame {
         let mut row_iter = sv.iter();
         let layout: [[u8; 9]; 9] = std::array::from_fn(|_| {
             let row = row_iter.next().unwrap();
-            let digits = row.chars().map(|x| x.to_digit(10).unwrap() as u8).collect::<Vec<u8>>();
+            let digits = row.chars().take(9).map(|x| x.to_digit(10).unwrap() as u8).collect::<Vec<u8>>();
             let res: [u8; 9] = digits.try_into().unwrap();
             res
         });
@@ -27,28 +28,78 @@ impl SudokuGame {
             .map(|c| c.to_vec())
             .collect();
         let squares: Vec<Vec<(usize, usize)>> = (0..9)
-            .map(|i| (i / 3 * 3, i % 3))
-            .flat_map(|(r, c)| (0..9).map(move |i| (r + i / 3, c + i % 3)))
+            .map(|i| (i / 3 * 3, (i % 3) * 3))
+            .flat_map(|(r, c)| (0..9).map(move |i| (r + (i / 3 % 3), c + i % 3)))
             .collect::<Vec<_>>()
             .chunks_exact(9)
             .map(|c| c.to_vec())
             .collect();
         let areas = [rows, columns, squares].concat();
 
+        let mut area_idx: [[Vec<usize>; 9]; 9] = Default::default();
+        
+        for (idx, area) in areas.iter().enumerate() {
+            for &(r, c) in area.iter() {
+                area_idx[r][c].push(idx);
+            }
+        }
+
         let mut used: Vec<u16> = vec![0; areas.len()];
 
         for (area, mask) in areas.iter().zip(used.iter_mut()) {
             *mask = area
                 .iter()
-                .map(|(r, c)| layout[*r][*c])
-                .filter(|x| *x != 0)
+                .map(|&(r, c)| layout[r][c])
+                .filter(|&x| x != 0)
                 .fold(0, |acc, x| acc | (1 << x));
         }
 
-        SudokuGame { layout, areas, used }
+        SudokuGame { layout, area_idx, areas, used }
     }
     
     pub fn solve(&mut self) -> Option<[[u8; 9]; 9]> {
+        let mut most_constrained: Option<(usize, usize)> = None;
+        for (idx, mask) in self.used.iter().enumerate() {
+            let free = (1..10).map(|x| (1 << x) & (mask)).filter(|&x| x == 0).count();
+            if free == 0 {
+                continue
+            }
+            if most_constrained.is_none() || most_constrained.unwrap().1 > free {
+                most_constrained = Some((idx, free));
+            }
+        }
+
+        if most_constrained.is_none() {
+            return Some(self.layout);
+        }
+
+        let &(r, c) = self.areas[most_constrained.unwrap().0].iter()
+            .find(|&&(r, c)| self.layout[r][c] == 0)
+            .unwrap();
+        for nv in 1..10 {
+            let mut sat = true;
+            for &ai in self.area_idx[r][c].iter() {
+                sat &= (self.used[ai] & (1 << nv)) == 0;
+            }
+            if !sat {
+                continue
+            }
+            self.layout[r][c] = nv;
+            for &ai in self.area_idx[r][c].iter() {
+                self.used[ai] ^= 1 << nv;
+            }
+
+            let res = self.solve();
+            if res.is_some() {
+                return res;
+            }
+
+            self.layout[r][c] = 0;
+            for &ai in self.area_idx[r][c].iter() {
+                self.used[ai] ^= 1 << nv;
+            }
+        }
+
         None
     }
 }
