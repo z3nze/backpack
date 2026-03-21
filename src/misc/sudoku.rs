05bfa98efba1e9e69f01d2f0467a9dd8bc6e377c
znz <z3nz@pm.me>
2026-03-21 21:16:04 +0400

    make sudoku solver iterator-heavy

--- src/misc/sudoku.rs
+++ src/misc/sudoku.rs
@@ -6,6 +6,35 @@ pub struct SudokuGame {
 }
 
 impl SudokuGame {
+    fn get_rows() -> Vec<Vec<(usize, usize)>> {
+        (0..9)
+            .flat_map(|i| std::iter::repeat_n(i, 9))
+            .zip((0..9).cycle())
+            .collect::<Vec<_>>()
+            .chunks_exact(9)
+            .map(|c| c.to_vec())
+            .collect()
+    }
+
+    fn get_columns() -> Vec<Vec<(usize, usize)>> {
+        (0..9).cycle()
+            .zip((0..9).flat_map(|i| std::iter::repeat_n(i, 9)))
+            .collect::<Vec<_>>()
+            .chunks_exact(9)
+            .map(|c| c.to_vec())
+            .collect()
+    }
+
+    fn get_squares() -> Vec<Vec<(usize, usize)>> {
+        (0..9)
+            .map(|i| (i / 3 * 3, (i % 3) * 3))
+            .flat_map(|(r, c)| (0..9).map(move |i| (r + (i / 3 % 3), c + i % 3)))
+            .collect::<Vec<_>>()
+            .chunks_exact(9)
+            .map(|c| c.to_vec())
+            .collect()
+    }
+
     pub fn from_string_vec(sv: Vec<String>) -> Self {
         let mut row_iter = sv.iter();
         let layout: [[u8; 9]; 9] = std::array::from_fn(|_| {
@@ -14,92 +43,59 @@ impl SudokuGame {
             let res: [u8; 9] = digits.try_into().unwrap();
             res
         });
-        let rows: Vec<Vec<(usize, usize)>> = (0..9)
-            .flat_map(|i| std::iter::repeat_n(i, 9))
-            .zip((0..9).cycle())
-            .collect::<Vec<_>>()
-            .chunks_exact(9)
-            .map(|c| c.to_vec())
-            .collect();
-        let columns: Vec<Vec<(usize, usize)>> = (0..9).cycle()
-            .zip((0..9).flat_map(|i| std::iter::repeat_n(i, 9)))
-            .collect::<Vec<_>>()
-            .chunks_exact(9)
-            .map(|c| c.to_vec())
-            .collect();
-        let squares: Vec<Vec<(usize, usize)>> = (0..9)
-            .map(|i| (i / 3 * 3, (i % 3) * 3))
-            .flat_map(|(r, c)| (0..9).map(move |i| (r + (i / 3 % 3), c + i % 3)))
-            .collect::<Vec<_>>()
-            .chunks_exact(9)
-            .map(|c| c.to_vec())
-            .collect();
-        let areas = [rows, columns, squares].concat();
+        let areas = [
+            Self::get_rows(),
+            Self::get_columns(),
+            Self::get_squares(),
+        ].concat();
 
         let mut area_idx: [[Vec<usize>; 9]; 9] = Default::default();
         
-        for (idx, area) in areas.iter().enumerate() {
-            for &(r, c) in area.iter() {
-                area_idx[r][c].push(idx);
-            }
-        }
+        areas.iter().enumerate().for_each(|(idx, area)| area.iter().for_each(|&(r, c)| area_idx[r][c].push(idx)));
 
-        let mut used: Vec<u16> = vec![0; areas.len()];
-
-        for (area, mask) in areas.iter().zip(used.iter_mut()) {
-            *mask = area
-                .iter()
-                .map(|&(r, c)| layout[r][c])
-                .filter(|&x| x != 0)
-                .fold(0, |acc, x| acc | (1 << x));
-        }
+        let used: Vec<u16> = areas.iter()
+            .map(|x| x.iter().map(|&(r, c)| layout[r][c]).filter(|&x| x != 0).fold(0, |acc, x| acc | (1 << x)))
+            .collect();
 
         SudokuGame { layout, area_idx, areas, used }
     }
+
+    pub fn flip(&mut self, pos: (usize, usize), val: u8) {
+        let (r, c) = pos;
+        self.layout[r][c] = val - self.layout[r][c];
+        self.area_idx[r][c].iter().for_each(|&idx| self.used[idx] ^= 1 << val);
+    }
     
     pub fn solve(&mut self) -> Option<[[u8; 9]; 9]> {
-        let mut most_constrained: Option<(usize, usize)> = None;
-        for (idx, mask) in self.used.iter().enumerate() {
-            let free = (1..10).map(|x| (1 << x) & (mask)).filter(|&x| x == 0).count();
-            if free == 0 {
-                continue
-            }
-            if most_constrained.is_none() || most_constrained.unwrap().1 > free {
-                most_constrained = Some((idx, free));
-            }
-        }
+        let most_constrained_area: Option<(usize, usize)> = self.used.iter().enumerate()
+            .map(|(idx, mask)| (idx, mask, (1..10).map(|x| (1 << x) & (mask)).filter(|&x| x == 0).count()))
+            .filter(|&(_idx, _mask, free)| free != 0)
+            .min_by(|x, y| x.2.cmp(&y.2))
+            .map(|(idx, _, free)| (idx, free));
 
-        if most_constrained.is_none() {
+        if most_constrained_area.is_none() {
             return Some(self.layout);
         }
 
-        let &(r, c) = self.areas[most_constrained.unwrap().0].iter()
+        let &(r, c) = self.areas[most_constrained_area.unwrap().0].iter()
             .find(|&&(r, c)| self.layout[r][c] == 0)
             .unwrap();
-        for nv in 1..10 {
-            let mut sat = true;
-            for &ai in self.area_idx[r][c].iter() {
-                sat &= (self.used[ai] & (1 << nv)) == 0;
-            }
-            if !sat {
-                continue
-            }
-            self.layout[r][c] = nv;
-            for &ai in self.area_idx[r][c].iter() {
-                self.used[ai] ^= 1 << nv;
-            }
 
-            let res = self.solve();
-            if res.is_some() {
-                return res;
-            }
+        let available_values = (1..10)
+            .map(|x| (1 << x) - self.area_idx[r][c].iter().fold(0, |acc, &y| acc | (self.used[y] & (1 << x))))
+            .fold(0, |acc, x| acc | x);
 
-            self.layout[r][c] = 0;
-            for &ai in self.area_idx[r][c].iter() {
-                self.used[ai] ^= 1 << nv;
-            }
-        }
-
-        None
+        (1..10)
+            .filter(|x| available_values & (1 << x) != 0)
+            .map(
+                |nv| 
+                {
+                    self.flip((r, c), nv); 
+                    if let Some(res) = self.solve() { return Some(res); };
+                    self.flip((r, c), nv);
+                    None
+                }
+            )
+            .find_map(|x| x)
     }
 }
