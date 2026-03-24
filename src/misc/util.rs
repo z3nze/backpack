fe62bdc7841be69a82a024e50a9199958368a921
znz <z3nz@pm.me>
2026-03-24 19:57:45 +0400

    carthesian product test

--- src/misc/util.rs
+++ src/misc/util.rs
@@ -8,3 +8,18 @@ pub fn prod<'a, T>(xs: &'a Vec<T>, ys: &'a Vec<T>) -> Vec<(&'a T, &'a T)> {
     }
     res
 }
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn test_prod() {
+        let a: Vec<i64> = vec![10, 20, 30];
+        let b: Vec<i64> = vec![50, 60, 70];
+
+        let expect: Vec<(i64, i64)> = vec![(10, 50), (10, 60), (10, 70), (20, 50), (20, 60), (20, 70), (30, 50), (30, 60), (30, 70)];
+
+        assert_eq!(prod(&a, &b).iter().map(|(x, y)| (**x, **y)).collect::<Vec<(i64, i64)>>(), expect);
+    }
+}
