5cde51ab15a71cd7da0d58542885326e91e3e443
znz <z3nz@pm.me>
2026-03-29 09:59:21 +0400

    add gcd tests

--- src/math/number_theory.rs
+++ src/math/number_theory.rs
@@ -4,3 +4,14 @@ pub fn gcd(a: i64, b: i64) -> i64 {
     }
     gcd(b % a, a)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    pub fn test_gcd() {
+        assert_eq!(gcd(24, 17), 1);
+        assert_eq!(gcd(259457, 2541), 11);
+    }
+}
