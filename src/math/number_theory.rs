ad219c0290a45d012693e15358d78887150b4866
znz <z3nz@pm.me>
2026-04-15 23:34:34 +0400

    euler totient function sqrt + test

--- src/math/number_theory.rs
+++ src/math/number_theory.rs
@@ -1,3 +1,5 @@
+#![allow(clippy::just_underscores_and_digits)]
+
 use crate::math::numerical::Integer;
 
 pub fn gcd<T: Integer>(a: T, b: T) -> T {
@@ -5,6 +7,30 @@ pub fn gcd<T: Integer>(a: T, b: T) -> T {
         return b;
     }
     gcd(b % a, a)
+}
+
+pub fn phi<T: Integer>(mut n: T) -> T {
+    let _0 = T::try_from(0).unwrap();
+    let _1 = T::try_from(1).unwrap();
+    let _2 = T::try_from(2).unwrap();
+
+    let mut result = n;
+    let mut i = _2;
+    while i * i <= n {
+        if n % i == _0 {
+            while n % i == _0 {
+                n /= i;
+            }
+            result -= result / i;
+        }
+        i += _1;
+    }
+
+    if n > _1 {
+        result -= result / n;
+    }
+
+    result
 }
 
 #[cfg(test)]
@@ -15,5 +41,11 @@ mod tests {
     pub fn test_gcd() {
         assert_eq!(gcd(24, 17), 1);
         assert_eq!(gcd(259457, 2541), 11);
+    }
+
+    #[test]
+    pub fn test_phi() {
+        assert_eq!(phi(11), 10);
+        assert_eq!(phi(18), 6);
     }
 }
