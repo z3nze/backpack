38588b5a6782d325216395c82873d9367fbaac10
znz <z3nz@pm.me>
2026-05-12 22:21:21 -0300

    geometry wip

--- src/math/geometry.rs
+++ src/math/geometry.rs
@@ -76,9 +76,30 @@ where
     }
 }
 
-impl<T> Polygon<T, 2> {
-    pub fn is_inside(point: Point<T, 2>) -> bool {
-        unimplemented!()
+impl<T, const N: usize> Polygon<T, N>
+where 
+    T: Numerical 
+{
+    fn new(points: Vec<Point<T, N>>) -> Self {
+        Polygon {
+            vertices: points,
+        }
+    }
+}
+
+impl<T> Polygon<T, 2>
+where 
+    T: Numerical
+{
+    pub fn is_inside(self, point: Point<T, 2>) -> bool {
+        let _0 = T::try_from(0).unwrap();
+        let edges = self.vertices.iter().zip(self.vertices.iter().cycle().skip(1));
+        let mut is_inside : bool = true;
+        for (&p1, &p2) in edges {
+            let triangle = Polygon::<T, 2>::new(vec![p1, p2, point]);
+            is_inside &= signed_triangle_area(triangle) >= _0
+        }
+        is_inside
     }
 }
 
@@ -87,13 +108,13 @@ pub fn cross<T: Numerical>(a: Vector<T, 2>, b: Vector<T, 2>) -> T {
 }
 
 pub fn signed_triangle_area<T: Numerical>(triangle: Polygon<T, 2>) -> T {
-    let _0 = T::try_from(0).unwrap();
+    let _2 = T::try_from(2).unwrap();
 
     let p0 = triangle.vertices[0];
     let p1 = triangle.vertices[1];
     let p2 = triangle.vertices[2];
 
-    cross(p1 - p0, p2 - p1)
+    cross(p1 - p0, p2 - p1) / _2
 }
 
 
