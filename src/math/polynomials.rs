3a6562637032ce477e1ac5e839a1c511a29070b9
znz <z3nz@pm.me>
2026-05-21 22:27:17 -0300

    lagrange interpolation implementation

--- src/math/polynomials.rs
+++ src/math/polynomials.rs
@@ -1,3 +1,4 @@
+#![allow(non_snake_case)]
 use crate::math::numerical::Numerical;
 
 pub struct LagrangeInterpolation<T>
@@ -17,5 +18,34 @@ where
             x: v.iter().map(|&(x, _)| x).collect::<Vec<_>>(),
             y: v.iter().map(|&(_, y)| y).collect::<Vec<_>>(),
         }
+    }
+
+    pub fn P(&self, x: T) -> T {
+        let n = self.x.len();
+        let mut P: T = T::lf_usize(0);
+        for i in 0..n {
+            let mut l: T = T::lf_usize(1);
+            for j in 0..n {
+                if i == j {
+                    continue
+                }
+                l = l * (x - self.x[j]) / (self.x[i] - self.x[j]);
+            }
+            P += self.y[i] * l;
+        }
+       P 
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    pub fn test_lagrange_interpolation() {
+        let points: Vec<(f64, f64)> = vec![(1.0, 1.0), (2.0, 8.0)];
+        let li = LagrangeInterpolation::new(points);
+
+        assert_eq!(li.P(3.0), 15.0);
     }
 }
