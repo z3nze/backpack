6cf25214a37e61311ab9df3784834fe0f37b88e5
znz <z3nz@pm.me>
2026-07-31 19:52:01 -0300

    edges test

--- src/graphs/graph.rs
+++ src/graphs/graph.rs
@@ -55,3 +55,25 @@ impl Edge {
         self.flow
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    pub fn test_edge() {
+        let new_non_weighted = Edge::new(0, 1);
+        assert_eq!(new_non_weighted.from(), 0);
+        assert_eq!(new_non_weighted.to(), 1);
+        assert_eq!(new_non_weighted.weight(), None);
+        assert_eq!(new_non_weighted.capacity(), None);
+        assert_eq!(new_non_weighted.flow(), None);
+
+        let new_weighted = Edge::new_weighted(3, 4, 10);
+        assert_eq!(new_weighted.from(), 3);
+        assert_eq!(new_weighted.to(), 4);
+        assert_eq!(new_weighted.weight(), Some(10));
+        assert_eq!(new_weighted.capacity(), None);
+        assert_eq!(new_weighted.flow(), None);
+    }
+}
